a13ea236994f5f0b253ac1278cada12329353f00
giovannipacelli2 <giovannipacelli2@gmail.com>
2023-07-22 19:26:17 +0200

    deploy

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,7 +1,7 @@
 import { createClient } from "contentful"; 
 
 export default createClient({
-    space: process.env.REACT_APP_API_SPACE,
-    accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+    space: process.env.REACT_APP_API_SPACE ? process.env.REACT_APP_API_SPACE : REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN ? process.env.REACT_APP_ACCESS_TOKEN : REACT_APP_ACCESS_TOKEN,
     
 });
