8f8b7bd4959566654aa470f29f58a0e4f7fc05b4
giovannipacelli2 <giovannipacelli2@gmail.com>
2023-07-22 19:14:05 +0200

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,8 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 //DOTENV
 const Dotenv = require( "dotenv-webpack" );
-const mode = process.env.NODE_ENV;
 /* const envPath = `.env.${process.env.NODE_ENV}`; */
-const envPath = mode!==undefined ? `.env.${mode}` : '.env';
+const envPath = '.env';
 require("dotenv").config({ path: envPath });
 
 module.exports = (env, argv) => {
