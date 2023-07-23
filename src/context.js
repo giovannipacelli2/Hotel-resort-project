1af3ad13d809712d8235061d632f9aaae919d370
giovannipacelli2 <giovannipacelli2@gmail.com>
2023-07-23 16:11:48 +0200

    prova2

--- src/context.js
+++ src/context.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import items from './data';
-import Client from './Contentful';
+/* import Client from './Contentful'; */
 
 const RoomContext = React.createContext();
 
@@ -25,12 +25,12 @@ class RoomProvider extends Component {
     //getData(){}
     getData = async ()=>{
       try {
-        let response = await Client.getEntries({
+        /* let response = await Client.getEntries({
           content_type: "beachResortRoom",
         });
 
-        let rooms = this.formatData([...response.items, ...items]);
-
+        let rooms = this.formatData([...response.items, ...items]); */
+        let rooms = this.formatData([...items]);
         let featuredRooms = rooms.filter( room => room.featured === true );
 
         let maxPrice = Math.max(...rooms.map(item => item.price));
